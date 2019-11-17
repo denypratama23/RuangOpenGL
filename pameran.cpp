c6adc0f5a8f24b71c93f4c61baa4bee38a46ad19
denypratama23 <denypratama23an@gmail.com>
2019-11-17 11:52:54 +0700

    jadi jendela depan
    jendela depan udah ada kayunya

--- pameran.cpp
+++ pameran.cpp
@@ -256,99 +256,99 @@ void kanvas(float x,float y,float z)
 
 //---------------gambar tulisan----------------//
 
-void tulisan(float x,float y,float z)
-{
+//void tulisan(float x,float y,float z)
+//{
 
 	
 	
-	glColor3f(0.8,0.4,0.2);
-	glBegin(GL_POLYGON);   //A
-	glVertex3f(x-33,y+12,z+5);
-	glVertex3f(x-37,y+5,z+5);
-	glVertex3f(x-38,y+5,z+5);
-	glVertex3f(x-35,y+20,z+5);
-	glVertex3f(x-31,y+20,z+5);
-	glVertex3f(x-28,y+5,z+5);
-	glVertex3f(x-29,y+5,z+5);
-	glEnd();
+//	glColor3f(0.8,0.4,0.2);
+//	glBegin(GL_POLYGON);   //A
+//	glVertex3f(x-33,y+12,z+5);
+//	glVertex3f(x-37,y+5,z+5);
+//	glVertex3f(x-38,y+5,z+5);
+//	glVertex3f(x-35,y+20,z+5);
+//	glVertex3f(x-31,y+20,z+5);
+//	glVertex3f(x-28,y+5,z+5);
+//	glVertex3f(x-29,y+5,z+5);
+//	glEnd();
 	
-	glColor3f(0.8,0.4,0.2);
-	glBegin(GL_POLYGON);  //R
-	glVertex3f(x-24,y+13,z+5);
-	glVertex3f(x-24,y+5,z+5);
-	glVertex3f(x-26,y+5,z+5);
-	glVertex3f(x-26,y+20,z+5);
-	glVertex3f(x-19,y+20,z+5);
-	glVertex3f(x-19,y+15,z+5);
-	glVertex3f(x-23,y+15,z+5);
-	glVertex3f(x-19,y+5,z+5);
-	glVertex3f(x-21,y+5,z+5);
-	glEnd();
+//	glColor3f(0.8,0.4,0.2);
+//	glBegin(GL_POLYGON);  //R
+///	glVertex3f(x-24,y+13,z+5);
+//	glVertex3f(x-24,y+5,z+5);
+//	glVertex3f(x-26,y+5,z+5);
+//	glVertex3f(x-26,y+20,z+5);
+//	glVertex3f(x-19,y+20,z+5);
+//	glVertex3f(x-19,y+15,z+5);
+//	glVertex3f(x-23,y+15,z+5);
+//	glVertex3f(x-19,y+5,z+5);
+//	glVertex3f(x-21,y+5,z+5);
+//	glEnd();
 	
-	glColor3f(0.8,0.4,0.2);
-	glBegin(GL_POLYGON);  //T
-	glVertex3f(x-13,y+16,z+5);
-	glVertex3f(x-17,y+16,z+5);
-	glVertex3f(x-17,y+20,z+5);
-	glVertex3f(x-7,y+20,z+5);
-	glVertex3f(x-7,y+16,z+5);
-	glVertex3f(x-11,y+16,z+5);
-	glVertex3f(x-11,y+5,z+5);
-	glVertex3f(x-13,y+5,z+5);
-	glEnd();
+//	glColor3f(0.8,0.4,0.2);
+//	glBegin(GL_POLYGON);  //T
+//	glVertex3f(x-13,y+16,z+5);
+//	glVertex3f(x-17,y+16,z+5);
+//	glVertex3f(x-17,y+20,z+5);
+//	glVertex3f(x-7,y+20,z+5);
+//	glVertex3f(x-7,y+16,z+5);
+//	glVertex3f(x-11,y+16,z+5);
+//	glVertex3f(x-11,y+5,z+5);
+//	glVertex3f(x-13,y+5,z+5);
+//	glEnd();
 	
-	glColor3f(0.3,0.015,0.13);//bawah
-	glBegin(GL_POLYGON);
-	glVertex3f(x+5,y,z+5);
-	glVertex3f(x-50,y,z+5);
-	glVertex3f(x-50,y+2,z+5);
-	glVertex3f(x+5,y+2,z+5);
-	glEnd();
+//	glColor3f(0.3,0.015,0.13);//bawah
+//	glBegin(GL_POLYGON);
+//	glVertex3f(x+5,y,z+5);
+//	glVertex3f(x-50,y,z+5);
+//	glVertex3f(x-50,y+2,z+5);
+//	glVertex3f(x+5,y+2,z+5);
+//	glEnd();
 	
-	glColor3f(0.3,0.015,0.13);//atas
-	glBegin(GL_POLYGON);
-	glVertex3f(x+5,y+23,z+5);
-	glVertex3f(x-50,y+23,z+5);
-	glVertex3f(x-50,y+25,z+5);
-	glVertex3f(x+5,y+25,z+5);
-	glEnd();
+//	glColor3f(0.3,0.015,0.13);//atas
+//	glBegin(GL_POLYGON);
+//	glVertex3f(x+5,y+23,z+5);
+//	glVertex3f(x-50,y+23,z+5);
+//	glVertex3f(x-50,y+25,z+5);
+//	glVertex3f(x+5,y+25,z+5);
+//	glEnd();
 	
-	glColor3f(0.3,0.015,0.13);//kiri
-	glBegin(GL_POLYGON);
-	glVertex3f(x-48,y,z+5);
-	glVertex3f(x-50,y,z+5);
-	glVertex3f(x-50,y+25,z+5);
-	glVertex3f(x-48,y+25,z+5);
-	glEnd();
+//	glColor3f(0.3,0.015,0.13);//kiri
+//	glBegin(GL_POLYGON);
+//	glVertex3f(x-48,y,z+5);
+//	glVertex3f(x-50,y,z+5);
+//	glVertex3f(x-50,y+25,z+5);
+//	glVertex3f(x-48,y+25,z+5);
+//	glEnd();
 	
-	glColor3f(0.3,0.015,0.13);//kanan
-	glBegin(GL_POLYGON);
-	glVertex3f(x+5,y,z+5);
-	glVertex3f(x+3,y,z+5);
-	glVertex3f(x+3,y+25,z+5);
-	glVertex3f(x+5,y+25,z+5);
-	glEnd();
+//	glColor3f(0.3,0.015,0.13);//kanan
+//	glBegin(GL_POLYGON);
+//	glVertex3f(x+5,y,z+5);
+//	glVertex3f(x+3,y,z+5);
+//	glVertex3f(x+3,y+25,z+5);
+//	glVertex3f(x+5,y+25,z+5);
+//	glEnd();
 	
-	glColor3f(0.3,0.015,0.13);
-	glBegin(GL_POLYGON);   //tali
-	glVertex3f(x-5,y+23,z+5);
-	glVertex3f(x-40,y+23,z+5);
-	glVertex3f(x-40,y+30,z+5);
-	glVertex3f(x-5,y+30,z+5);
-	glEnd();
+//	glColor3f(0.3,0.015,0.13);
+//	glBegin(GL_POLYGON);   //tali
+//	glVertex3f(x-5,y+23,z+5);
+//	glVertex3f(x-40,y+23,z+5);
+//	glVertex3f(x-40,y+30,z+5);
+//	glVertex3f(x-5,y+30,z+5);
+//	glEnd();
 	
 	
 	
-	glColor3f(0.8,0.6,0.4);//kanvas tulisan
-	glBegin(GL_POLYGON);
-	glVertex3f(x+5,y,z+4.9);
-	glVertex3f(x-50,y,z+4.9);
-	glVertex3f(x-50,y+25,z+4.9);
-	glVertex3f(x+5,y+25,z+4.9);
-	glEnd();
+//	glColor3f(0.8,0.6,0.4);//kanvas tulisan
+//	glBegin(GL_POLYGON);
+//	glVertex3f(x+5,y,z+4.9);
+//	glVertex3f(x-50,y,z+4.9);
+//	glVertex3f(x-50,y+25,z+4.9);
+//	glVertex3f(x+5,y+25,z+4.9);
+//	glEnd();
 	
 
-}
+//}
 
 //---------------gambar frame jendela----------------//
 
@@ -441,13 +441,13 @@ void house1()
 	jendela(200,30,195.2); //jendela 1.4
 	
 	//-----------jendela belakang rumah--------------//
-	jendela(-150,30,-205.2); //jendela 2.-1
-	jendela(-50,30,-205.2); //jendela 2.-2
-	jendela(100,30,-205.2); //jendela 2.-3
-	jendela(200,30,-205.2); //jendela 2.-4
+	//jendela(-150,30,-205.2); //jendela 2.-1
+	//jendela(-50,30,-205.2); //jendela 2.-2
+	//jendela(100,30,-205.2); //jendela 2.-3
+	//jendela(200,30,-205.2); //jendela 2.-4
 
 	//-----------tulisan depan rumah--------------//
-	tulisan(20,60,195.2); //jendela 1.1
+	//tulisan(20,60,195.2); //jendela 1.1
 
 
 	kanvas(-150,30,195.1); //kanvas 1.1
